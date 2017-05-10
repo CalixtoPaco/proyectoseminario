b9a0d16b1cca7ff621e0c69e8c231ea325ec3f50
Calixto Paco <calixtopv@gmail.com>
2017-05-10 17:40:31 -0400

    diseniando la pagina del curso mas el ruteo

--- client/main.js
+++ client/main.js
@@ -1,8 +1,47 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+Template.info.onRendered(function() {
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+});
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+
+
+var chip = {
+    tag: 'chip content',
+    image: '', //optional
+    id: 1, //optional
+};
+
+
+$('.chips-initial').material_chip({
+    data: [{
+        tag: 'Apple',
+    }, {
+        tag: 'Microsoft',
+    }, {
+        tag: 'Google',
+    }],
+});
+$('.chips-placeholder').material_chip({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+});
+$('.chips-autocomplete').material_chip({
+    autocompleteOptions: {
+        data: {
+            'Apple': null,
+            'Microsoft': null,
+            'Google': null
+        },
+        limit: Infinity,
+        minLength: 1
+    }
+});
