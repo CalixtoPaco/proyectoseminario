43ed9d9c43d3065a63dc47fd5fa82c9821703d54
Calixto Paco <calixtopv@gmail.com>
2017-05-19 10:26:43 -0400

    arreglado la master

--- client/main.js
+++ client/main.js
@@ -14,40 +14,8 @@ Template.info.onRendered(function() {
 
 
 
-var chip = {
+/*var chip = {
     tag: 'chip content',
     image: '', //optional
     id: 1, //optional
-};
-
-
-<<<<<<< HEAD
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-//hola
-=======
-$('.chips-initial').material_chip({
-    data: [{
-        tag: 'Apple',
-    }, {
-        tag: 'Microsoft',
-    }, {
-        tag: 'Google',
-    }],
-});
-$('.chips-placeholder').material_chip({
-    placeholder: 'Enter a tag',
-    secondaryPlaceholder: '+Tag',
-});
-$('.chips-autocomplete').material_chip({
-    autocompleteOptions: {
-        data: {
-            'Apple': null,
-            'Microsoft': null,
-            'Google': null
-        },
-        limit: Infinity,
-        minLength: 1
-    }
-});
->>>>>>> ba1023c2f34c795282936e91e6f3e3e3abba96d4
+};*/
