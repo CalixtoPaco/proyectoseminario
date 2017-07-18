21623efb12a9931ac0a18a5ea587fed5c48f8282
Calixto Paco <calixtopv10@gmail.com>
2017-07-18 09:51:18 -0400

    corrigiendo repeticion de videos

--- client/cursos/form/video.js
+++ client/cursos/form/video.js
@@ -1,8 +1,15 @@
+materialid="";
+Template.materialcurso.events({
+	'click a': function(e){
+		console.log(e.target.name);
+		materialid=e.target.name;
+	}
+});
 Template.video.onCreated(function(){
     Meteor.subscribe('data');
 });
 Template.video.helpers({
     cargarvideo: function(){
-        return Material.find();
+        return Material.find({_id:materialid});
     }
 });
