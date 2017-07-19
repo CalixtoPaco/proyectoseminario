6c31e079eb0dfe379e3ba436a5228df890dafdae
Calixto Paco <calixtopv10@gmail.com>
2017-07-19 10:48:14 -0400

    chat modificado

--- client/cursos/chat/chatsms.js
+++ client/cursos/chat/chatsms.js
@@ -16,18 +16,18 @@ Template.temaus.events({
     } 
 });
 Template.chatsms.helpers({
-  michat (){
-    console.log(CHAT.find());
-    return CHAT.find({idDestination: idmatchat});
-    //Meteor.subscribe('micurso', Accounts.user()._id);
-  },
+      michat (){
+        console.log(CHAT.find());
+        return CHAT.find({idDestination: idmatchat});
+        //Meteor.subscribe('micurso', Accounts.user()._id);
+      },
     idMe: function(){
-		console.log(this.idSource);
-		console.log(this.idSource == Accounts.user()._id);
+		//console.log(this.idSource);
+		//onsole.log(this.idSource == Accounts.user()._id);
 		return this.idSource == Accounts.user()._id;
 	},
     nombre: function(){
-		console.log(this);
+		//console.log('hola');
 		console.log(Meteor.users.findOne({_id:this.idSource}));
 		return Meteor.users.findOne({_id:this.idSource});
 	}
